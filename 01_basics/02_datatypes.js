d2b4667a7ba0a5c3e6c636ab6943e782f1d93545
Gauravkumaryadav4321 <123934871+Gauravkumaryadav4321@users.noreply.github.com>
2024-09-14 21:01:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"// treat all js code as newer version
+
+//alert("3+3")// we are using node js ,not browser
+
+console.log(3+
+    
+    3)// code readilibility should be good
+console.log("gaurab")
+
+
+let name ="hitesh"//string
+let age=18//number
+let isloggedIn = false
+let state=null
+
+
+
+//number=>2to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+
+//object
+
+
+console.log( typeof  age)///define types of varible using
