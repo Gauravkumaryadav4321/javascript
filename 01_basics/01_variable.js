15eadf01f2429efc61b04bf4611e2d84b6fdcc1e
Gauravkumaryadav4321 <123934871+Gauravkumaryadav4321@users.noreply.github.com>
2024-09-14 20:33:57 +0000

    developer.java

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword ="12345"
+accountCity="Jaipur"
+let accountstate;
+
+//accountId=2
+accountEmail= "hs@hc.com"
+acccountPassword="1232324"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+
+
