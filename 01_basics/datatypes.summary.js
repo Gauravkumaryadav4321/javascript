306792a8304c14f65df7fb674ca92a52b1ed2ec3
Gauravkumaryadav4321 <123934871+Gauravkumaryadav4321@users.noreply.github.com>
2024-09-14 22:54:30 +0000

    datatypes summary

---
+++ 01_basics/datatypes.summary.js
@@ -0,0 +1,31 @@
+// primitive
+
+/// 7 types: String, Number,Boolean,null,symbol,BigInt
+
+//const score =100
+const scoreValue=100.3
+const isLoggedin=false
+const outsideTemp=null
+let userEmail;
+
+const id =Symbol ('123')
+const anotherId=Symbol('123')
+
+const bigNumber=23943494548934n
+
+
+
+
+//refrence (non primitive)
+///array,objects, function
+
+
+const heros =["salman","ritik","ranbir"]
+
+ const myFunction =function(){
+    console.log("hello world");
+    
+ }
+
+ console.log(typeof bignumber);
+ 
